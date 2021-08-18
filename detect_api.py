d5dfb67c992bb4bbc81a4878a52cb28148c2b645
Rahul <vishwakarma.rhl@gmail.com>
2021-08-18 16:52:42 -0700

    updated the detection api codebase

--- detect_api.py
+++ detect_api.py
@@ -20,29 +20,30 @@ sys.path.append(FILE.parents[0].as_posix())  # add yolov5/ to path
 from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages, letterbox
 from utils.general import check_img_size, check_requirements, check_imshow, colorstr, non_max_suppression, \
-    apply_classifier, scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path, save_one_box
-from utils.plots import colors, plot_one_box
-from utils.torch_utils import select_device, load_classifier, time_sync
+    apply_classifier, scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized
 
+device, model, class_names = 'cpu', None, None # cuda device, i.e. 0 or 0,1,2,3 or cpu
 """ Initialize the model weights """
-def init_model(weights='yolov5s.pt', 
-               device='cpu'             # cuda device, i.e. 0 or 0,1,2,3 or cpu
-               ):
+def init_model(weights='yolov5s.pt'):
+    global device, model, class_names
     # Load model
     device = select_device(device)
     model = attempt_load(weights, map_location=device)   # load FP32 model
     # Get names and colors
-    names = model.module.names if hasattr(model, 'module') else model.names
+    class_names = model.module.names if hasattr(model, 'module') else model.names
     #colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(names))]
-    print("Initialized Model. \nClass names: ", names, " \n")
-    return device, model, names
-# Init model
-device, model, class_names = init_model(weights="/home/rahul/workspace/mmm/sura/detector/yolov5/yolov5s.pt")
+    print("Initialized Model. \nClass names: ", class_names, " \n")
+    return device, model, class_names
 
 """ loads an image from dataset, returns img, original hw, resized hw """
 def load_image(image_path, img_size=640):
-    img0 = cv2.imread(image_path)     # BGR
-    assert img0 is not None, 'Image Not Found ' + path
+    if type(image_path) == str:
+        img0 = cv2.imread(image_path)     # load file path as cv2 BGR
+    else: 
+        img0 = image_path                 # cv2 loaded image assigned
+    assert img0 is not None, 'Image Not Found ' + image_path
     h0, w0 = img0.shape[:2]  # orig hw
     r = img_size / max(h0, w0)  # resize image to img_size
     # Padded resize
@@ -54,31 +55,20 @@ def load_image(image_path, img_size=640):
 
 """ Format the detection results """
 def format_detect_result(path, img_og, img, preds, names):
-    formatted_result, s = ["{}".format(path)], ""
+    detection_results = []
     # Process detections per image
     for i, det in enumerate(preds):
         #print(i, " ---> ")
         if det is not None and len(det):
             # Rescale boxes from img_size to img0 size
             det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img_og.shape).round()
-            # Print results
-            for c in det[:, -1].detach().unique():
-                n = (det[:, -1] == c).sum()  # detections per class
-                s += '%g %ss, ' % (n, names[int(c)])  # add to string
-            score_dict = {}
             # Write results
-            for *xyxy, conf, cls in reversed(det):
+            for *xyxy, conf, clf in reversed(det):
                 conf = conf.detach().numpy()
                 (xmin, ymin, xmax, ymax) = torch.tensor(xyxy).view(1, 4).numpy()[0]
-                label = '%.6f' % (conf)     
-                # class_submission_id, x_min, y_min, x_max, y_max, confidence
-                out_str = "{0} {1} {2} {3} {4} {5}".format(int(cls)+1, int(xmin), int(ymin), int(xmax), int(ymax), label)
-                score_dict[label] = out_str
-            for key in sorted(score_dict.keys(), reverse=True):
-                #print(key)
-                formatted_result.append(score_dict[key])
-    print('[Result] - %s' % (s))
-    return formatted_result
+                box_list = list(map(int, [xmin, ymin, xmax, ymax]))
+                detection_results.append({"rect": box_list, "conf": float(conf),"cls": int(clf)})
+    return detection_results
 
 @torch.no_grad()
 def detect( image, 
@@ -86,7 +76,6 @@ def detect( image,
             imgsz=640,              # inference size (pixels)
             conf_thres=0.25,        # confidence threshold
             iou_thres=0.45,         # NMS IOU threshold
-            max_det=1000,           # maximum detections per image
             classes=None,           # filter by class: --class 0, or --class 0 2 3
             agnostic_nms=False,     # class-agnostic NMS
             augment=False,          # augmented inference
@@ -99,13 +88,18 @@ def detect( image,
         image_names.append(image)
         img0, img_hw0, img, _ = load_image(image_path=image, img_size=imgsz)
         img0s.append(img0); img_hw0s.append(img_hw0); imgs.append(img)
-    if type(image) == list:
+    elif type(image) == list:
         for image_item in image:
             image_names.append(os.path.basename(image_item))
             img0, img_hw0, img, _ = load_image(image_path=image_item, img_size=imgsz)
             img0s.append(img0); img_hw0s.append(img_hw0); imgs.append(img)
+    else:
+        image_names.append(source)
+        img0, img_hw0, img, _ = load_image(image_path=image, img_size=imgsz)
+        img0s.append(img0); img_hw0s.append(img_hw0); imgs.append(img)
+    
     # Process
-    formatted_results = []
+    preds, formatted_results = [], []
     t0 = time.time()
     for path, img0, img_hw0, img in zip(image_names, img0s, img_hw0s, imgs):
         stride = int(model.stride.max())  # model stride
@@ -116,15 +110,15 @@ def detect( image,
         if img.ndimension() == 3:
             img = img.unsqueeze(0)
         # Inference
-        t1 = time_sync()
+        t1 = time_synchronized()
         pred = model(img, augment=augment)[0]
         # Apply NMS
-        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
-        elapsed = time_sync() - t1
+        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms)
+        elapsed = time_synchronized() - t1
         formatted_results.append(format_detect_result(path, img0, img, pred, class_names))
         print('Time [Inference + NMS]: (%.3fs)' % (elapsed))
     print("\nAll Detections: {}".format(formatted_results))
-    return pred, formatted_results
+    return formatted_results
     
 @torch.no_grad()
 def stream( source="http://192.168.4.25:8080/video",   # file/dir/URL/glob, 0 for webcam
@@ -162,11 +156,11 @@ def stream( source="http://192.168.4.25:8080/video",   # file/dir/URL/glob, 0 fo
         if img.ndimension() == 3:
             img = img.unsqueeze(0)
         # Inference
-        t1 = time_sync()
+        t1 = time_synchronized()
         pred = model(img, augment=augment)[0]
         # Apply NMS
         pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
-        elapsed = time_sync() - t1
+        elapsed = time_synchronized() - t1
         
         # Process predictions
         for i, det in enumerate(pred):  # detections per image
@@ -201,215 +195,6 @@ def stream( source="http://192.168.4.25:8080/video",   # file/dir/URL/glob, 0 fo
         print(f'Done. ({time.time() - t0:.3f}s)')
     return pred
 
-@torch.no_grad()
-def run(weights='yolov5s.pt',  # model.pt path(s)
-        source='data/images',  # file/dir/URL/glob, 0 for webcam
-        imgsz=640,  # inference size (pixels)
-        conf_thres=0.25,  # confidence threshold
-        iou_thres=0.45,  # NMS IOU threshold
-        max_det=1000,  # maximum detections per image
-        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-        view_img=False,  # show results
-        save_txt=False,  # save results to *.txt
-        save_conf=False,  # save confidences in --save-txt labels
-        save_crop=False,  # save cropped prediction boxes
-        nosave=False,  # do not save images/videos
-        classes=None,  # filter by class: --class 0, or --class 0 2 3
-        agnostic_nms=False,  # class-agnostic NMS
-        augment=False,  # augmented inference
-        visualize=False,  # visualize features
-        update=False,  # update all models
-        project='runs/detect',  # save results to project/name
-        name='exp',  # save results to project/name
-        exist_ok=False,  # existing project/name ok, do not increment
-        line_thickness=3,  # bounding box thickness (pixels)
-        hide_labels=False,  # hide labels
-        hide_conf=False,  # hide confidences
-        half=False,  # use FP16 half-precision inference
-        ):
-    save_img = not nosave and not source.endswith('.txt')  # save inference images
-    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
-        ('rtsp://', 'rtmp://', 'http://', 'https://'))
-
-    # Directories
-    save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
-    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
-
-    # Initialize
-    set_logging()
-    device = select_device(device)
-    half &= device.type != 'cpu'  # half precision only supported on CUDA
-
-    # Load model
-    w = weights[0] if isinstance(weights, list) else weights
-    classify, pt, onnx = False, w.endswith('.pt'), w.endswith('.onnx')  # inference type
-    stride, names = 64, [f'class{i}' for i in range(1000)]  # assign defaults
-    if pt:
-        model = attempt_load(weights, map_location=device)  # load FP32 model
-        stride = int(model.stride.max())  # model stride
-        names = model.module.names if hasattr(model, 'module') else model.names  # get class names
-        if half:
-            model.half()  # to FP16
-        if classify:  # second-stage classifier
-            modelc = load_classifier(name='resnet50', n=2)  # initialize
-            modelc.load_state_dict(torch.load('resnet50.pt', map_location=device)['model']).to(device).eval()
-    elif onnx:
-        check_requirements(('onnx', 'onnxruntime'))
-        import onnxruntime
-        session = onnxruntime.InferenceSession(w, None)
-    imgsz = check_img_size(imgsz, s=stride)  # check image size
-
-    # Dataloader
-    if webcam:
-        view_img = check_imshow()
-        cudnn.benchmark = True  # set True to speed up constant image size inference
-        dataset = LoadStreams(source, img_size=imgsz, stride=stride)
-        bs = len(dataset)  # batch_size
-    else:
-        dataset = LoadImages(source, img_size=imgsz, stride=stride)
-        bs = 1  # batch_size
-    vid_path, vid_writer = [None] * bs, [None] * bs
-
-    # Run inference
-    if pt and device.type != 'cpu':
-        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
-    t0 = time.time()
-    for path, img, im0s, vid_cap in dataset:
-        if pt:
-            img = torch.from_numpy(img).to(device)
-            img = img.half() if half else img.float()  # uint8 to fp16/32
-        elif onnx:
-            img = img.astype('float32')
-        img /= 255.0  # 0 - 255 to 0.0 - 1.0
-        if len(img.shape) == 3:
-            img = img[None]  # expand for batch dim
-
-        # Inference
-        t1 = time_sync()
-        if pt:
-            visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
-            pred = model(img, augment=augment, visualize=visualize)[0]
-        elif onnx:
-            pred = torch.tensor(session.run([session.get_outputs()[0].name], {session.get_inputs()[0].name: img}))
-
-        # NMS
-        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
-        t2 = time_sync()
-
-        # Second-stage classifier (optional)
-        if classify:
-            pred = apply_classifier(pred, modelc, img, im0s)
-
-        # Process predictions
-        for i, det in enumerate(pred):  # detections per image
-            if webcam:  # batch_size >= 1
-                p, s, im0, frame = path[i], f'{i}: ', im0s[i].copy(), dataset.count
-            else:
-                p, s, im0, frame = path, '', im0s.copy(), getattr(dataset, 'frame', 0)
-
-            p = Path(p)  # to Path
-            save_path = str(save_dir / p.name)  # img.jpg
-            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
-            s += '%gx%g ' % img.shape[2:]  # print string
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-            imc = im0.copy() if save_crop else im0  # for save_crop
-            if len(det):
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
-
-                # Print results
-                for c in det[:, -1].unique():
-                    n = (det[:, -1] == c).sum()  # detections per class
-                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
-
-                # Write results
-                for *xyxy, conf, cls in reversed(det):
-                    if save_txt:  # Write to file
-                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
-                        with open(txt_path + '.txt', 'a') as f:
-                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
-
-                    if save_img or save_crop or view_img:  # Add bbox to image
-                        c = int(cls)  # integer class
-                        label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
-                        plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_thickness=line_thickness)
-                        if save_crop:
-                            save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
-
-            # Print time (inference + NMS)
-            print(f'{s}Done. ({t2 - t1:.3f}s)')
-
-            # Stream results
-            if view_img:
-                cv2.imshow(str(p), im0)
-                cv2.waitKey(1)  # 1 millisecond
-
-            # Save results (image with detections)
-            if save_img:
-                if dataset.mode == 'image':
-                    cv2.imwrite(save_path, im0)
-                else:  # 'video' or 'stream'
-                    if vid_path[i] != save_path:  # new video
-                        vid_path[i] = save_path
-                        if isinstance(vid_writer[i], cv2.VideoWriter):
-                            vid_writer[i].release()  # release previous video writer
-                        if vid_cap:  # video
-                            fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        else:  # stream
-                            fps, w, h = 30, im0.shape[1], im0.shape[0]
-                            save_path += '.mp4'
-                        vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
-                    vid_writer[i].write(im0)
-
-    if save_txt or save_img:
-        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
-        print(f"Results saved to {save_dir}{s}")
-
-    if update:
-        strip_optimizer(weights)  # update model (to fix SourceChangeWarning)
-
-    print(f'Done. ({time.time() - t0:.3f}s)')
-
-
-def parse_opt():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='data/images', help='file/dir/URL/glob, 0 for webcam')
-    parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
-    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='show results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
-    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--visualize', action='store_true', help='visualize features')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
-    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
-    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
-    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
-    opt = parser.parse_args()
-    return opt
-
-
-def main(opt):
-    print(colorstr('detect: ') + ', '.join(f'{k}={v}' for k, v in vars(opt).items()))
-    check_requirements(exclude=('tensorboard', 'thop'))
-    run(**vars(opt))
-
 
 if __name__ == "__main__":
-    opt = parse_opt()
-    main(opt)
+    pass
